5198b0d5dfcd659218d39d228404420e1c53288a
Mehdi Asadli <mehdiasadli@azericard.com>
2025-05-29 16:35:05 +0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,35 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['@prisma/client', '@prisma/engines'],
+  webpack(config, { webpack, isServer }) {
+    if (isServer) {
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          resourceRegExp:
+            /^(lokijs|mongodb-client-encryption|@aws-sdk\/credential-providers|@aws-sdk\/client-sso-oidc|snappy|aws4|mongodb-connection-string-url|kerberos|@mongodb-js\/zstd|gcp-metadata|socks|aws-crt|@azure\/msal-node|@azure\/identity|pg-native)$/,
+        })
+      );
+    }
+
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: false,
+      stream: false,
+      url: false,
+      zlib: false,
+      http: false,
+      https: false,
+      assert: false,
+      os: false,
+      path: false,
+    };
+
+    return config;
+  },
   async headers() {
     return [
       {
